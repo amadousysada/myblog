47eefecc87757121b22ce5499fcc816a5010508d
ASy <amsada93@gmail.com>
2019-05-06 16:57:06 +0200

    countdown

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.contrib import messages
 from django.shortcuts import render
+import datetime
+
 
 # Create your views here.
 
@@ -9,10 +11,13 @@ def home_view(request):
     messages.add_message(request, messages.INFO, 'Hello world.')
     messages.add_message(request,messages.ERROR, "fuck")
     messages.error(request, 'Email box full', extra_tags='email')"""
-
+    countdownStart = datetime.datetime(2019, 5, 10,00, 00)
+    countdownNow = countdownStart-datetime.datetime.now()
+    date = computeTimeStamp(countdownNow.seconds)
     return render(
         request,
         'blog/home.html',
+        {"date":date,"countdown":countdownNow}
     )
 
 def about_view(request):
@@ -25,3 +30,22 @@ def about_view(request):
         request,
         'blog/about.html',
     )
+
+def computeTimeStamp(countdown):
+
+    hours = countdown // 3600 if (countdown // 3600)>9 else "0"+str((countdown // 3600))
+
+    minutes = (countdown % 3600)//60 if (countdown // 60)>9 else "0"+str((countdown // 60))
+
+    seconds = (countdown % 3600)%60 if ((countdown % 3600)%60)>9 else "0"+str((countdown % 3600)%60)
+
+    attributes = { 'hours': hours, 'minutes': minutes, 'seconds': seconds }
+    date = dateObject(attributes)
+    return date
+
+class dateObject():
+    """docstring for ClassName"""
+    def __init__(self, initial_attrs):
+        for key in initial_attrs:
+            setattr(self, key, initial_attrs[key])
+        
--- myblog/settings.py
+++ myblog/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog',
     'bootstrap4',
+    'mathfilters',
 ]
 
 MIDDLEWARE = [
