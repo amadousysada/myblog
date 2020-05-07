1610243c5fa12ae33d2c770e1d7f5607481e5c71
amadsy <amadsy@LFR033150>
2020-05-07 05:08:26 +0200

    static_root

--- myblog/settings.py
+++ myblog/settings.py
@@ -141,6 +141,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 #STATIC_ROOT = BASE_DIR+"/var/www/amadou.com/static/"
 
 
